dadb27e1b099d41682bd7b3c1e1986e50dc1c2cb
Lucas Lopez <lucas.lopez.tr@gmail.com>
2025-11-13 00:35:53 -0500

    Initial Commit

---
+++ Wordish/WordishApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordishApp.swift
+//  Wordish
+//
+//  Created by Lucas Lopez on 11/13/25.
+//
+
+import SwiftUI
+
+@main
+struct WordishApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
